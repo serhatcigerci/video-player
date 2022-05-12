c58def40a72e2f5d3cb0068bc14d82a547d9c0d4
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-12 19:01:34 +0300

    opacity at bottom

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const playPauseBtn = document.querySelector('.play-pause-btn')
 const video = document.querySelector('video')
 const videoContainer = document.querySelector('.video-container')
 playPauseBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
 
+document.addEventListener('keydown', e => {
+  switch (e.key.toLowerCase()) {
+    case ' ':
+      case 'k':
+        togglePlay()
+        break
+  }
+})
 
 function togglePlay() {
   video.paused ? video.play() : video.pause()
