3fa253218dcbd87eaf9725a6253abc71b74608d5
serhatcigerci <yserhatcigerci@gmail.com>
2022-05-12 18:55:46 +0300

    fixing missing .

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const playPauseBtn = document.querySelector('.play-pause-btn')
 const video = document.querySelector('video')
+const videoContainer = document.querySelector('.video-container')
 playPauseBtn.addEventListener('click', togglePlay)
 
 
 function togglePlay() {
   video.paused ? video.play() : video.pause()
 }
+
+video.addEventListener('play', () => {
+  videoContainer.classList.remove('paused')
+})
+
+video.addEventListener('pause', () => {
+  videoContainer.classList.add('paused')
+})
